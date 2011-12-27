9c0fc7d276406cad8d9423f314cd7284ee303c34
Thomas Shaddox <wediddoit@gmail.com>
2011-12-27 00:33:18 -0600

    Oops, remove previous files.

--- mwc/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mwc::Application.config.secret_token = '42d3d65a69ae35cbb0f9a87cc3d444e44ea9c0f8e8d31084f8afda4098c3d7f336dc20645d24608cc1309c20982abaaadbed0c44a7027b24e40a8de1ee965aaf'
