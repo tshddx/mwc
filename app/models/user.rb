1cfcd8558e44f137dda2738f7fe0f78797cc652a
Thomas Shaddox <wediddoit@gmail.com>
2011-12-27 21:49:01 -0600

    Edit user and forms to show separate first and last names.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
   validates :calling_list_id, :uniqueness => true, :allow_nil => true
 
