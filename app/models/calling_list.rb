b77f8a795f936c689bc7693708dc977ee1bf40c5
Thomas Shaddox <wediddoit@gmail.com>
2011-12-29 16:27:25 -0600

    More CallingList and Member association nonsense.

--- app/models/calling_list.rb
+++ app/models/calling_list.rb
@@ -1,6 +1,6 @@
 class CallingList < ActiveRecord::Base
   has_many :members
-  belongs_to :owner, :class_name => "Member"
+  belongs_to :owner, :class_name => "Member", :foreign_key => "member_id"
 
-  validates :member_id, :presence => true
+  validates :member_id, :presence => true, :uniqueness => true
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,13 +1,11 @@
 class Member < ActiveRecord::Base
-  has_one :calling_list
+  belongs_to :calling_list
   
   has_many :meeting_reservations, :class_name => "Reservation"
   has_many :meetings_reserved, :through => :meeting_reservations, :source => :meeting
   
   has_many :meeting_attendeds, :class_name => "MeetingAttended"
   has_many :meetings_attended, :through => :meeting_attendeds, :source => :meeting
-
-  validates :calling_list_id, :uniqueness => true, :allow_nil => true
 
   def name
     "#{first_name} #{last_name}"
